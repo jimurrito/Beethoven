c94505910de80f0f859724bde7a95db27d42d7bb
jimurrito <jimurrito@gmail.com>
2025-04-18 20:10:23 -0500

    Added Public API config.

--- src/lib/beethoven.ex
+++ src/lib/beethoven.ex
@@ -8,23 +8,126 @@ defmodule Beethoven do
   This module acts as the unified client for interacting with beethoven as an external client.
   Avoid using the other modules for external PID calls to beethoven services.
   """
-  
-  alias Beethoven.Role.Client, as: RoleClient
-  alias Beethoven.RoleAlloc.Client, as: RoleAlloc
+
+  alias Beethoven.CoreServer
+  alias Beethoven.RoleServer
+
+  #
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  #
+  # Types
+  #
+
+  #
+  @typedoc """
+  List of node names.
+  """
+  @type nodeList() :: list(node())
+
+  #
+  @typedoc """
+  Status of a Beethoven cluster node.
+  """
+  @type nodeStatusMap() :: %{
+          node: node(),
+          status: CoreServer.nodeStatus(),
+          last_change: DateTime
+        }
+
+  #
+  @typedoc """
+  List of `nodeStatus()` objects.
+  """
+  @type nodeStatusMapList :: list(nodeStatusMap())
+
+  #
+  @typedoc """
+  Role data structure.
+  """
+  @type role() :: %{role: atom(), count: integer(), assigned: integer(), nodes: nodeList()}
+
+  #
+  @typedoc """
+  List of `role()`.
+  """
+  @type roleList() :: list(role())
+
+  #
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  #
+  # Holistic external client API for Beethoven.
+  #
 
   #
   #
   @doc """
-  Kills the an Elixir/Erlang application running Beethoven.
+  Returns state of this node within the Beethoven cluster.
+  Return will only be `:clustered` or `:standalone`.
   """
-  @spec kill_node() :: :ok
-  def kill_node() do
-    # Prune self from Tracker via RoleAlloc Server
-    :ok = RoleAlloc.prune()
-    # Kill all roles on the RoleServer
-    :ok = RoleClient.kill_all_roles()
-    # Kills ErlangVM
-    :ok = :init.stop()
+  @spec get_node_status() :: CoreServer.serverStatus()
+  def get_node_status() do
+    CoreServer.get_mode()
+  end
+
+  #
+  #
+  @doc """
+  Returns all active cluster nodes by their Node name URI.
+  Returns an empty list if this node is in `:standalone`.
+  """
+  @spec get_active_nodes() :: nodeList()
+  def get_active_nodes() do
+    Node.list()
+  end
+
+  #
+  #
+  @doc """
+  Returns all cluster nodes and their state.
+  """
+  @spec get_cluster_nodes() :: nodeStatusMapList()
+  def get_cluster_nodes() do
+    tableName = CoreServer.Tracker
+    #
+    {:atomic, return} =
+      fn ->
+        :mnesia.select(tableName, [
+          {
+            {tableName, :"$1", :"$2", :"$3"},
+            [],
+            [%{node: :"$1", status: :"$2", last_change: :"$3"}]
+          }
+        ])
+      end
+      |> :mnesia.transaction()
+
+    #
+    return
+  end
+
+  #
+  #
+  @doc """
+  Returns all roles hosted in Beethoven.
+  """
+  @spec get_roles() :: roleList()
+  def get_roles() do
+    tableName = RoleServer.Tracker
+    #
+    {:atomic, return} =
+      fn ->
+        :mnesia.select(tableName, [
+          {
+            {tableName, :"$1", :"$2", :"$3", :"$4", :_},
+            [],
+            [%{role: :"$1", count: :"$2", assigned: :"$3", nodes: :"$4"}]
+          }
+        ])
+      end
+      |> :mnesia.transaction()
+
+    #
+    return
   end
 
   #
