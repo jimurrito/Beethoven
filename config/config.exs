ed8b625d91cb434d3487d2f0eab3e7cc1eecc625
jimurrito <jimurrito@gmail.com>
2025-06-18 11:09:33 -0500

    Updated README

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,8 @@ config :beethoven,
   cluster_net: "127.0.0.0",
   cluster_net_mask: "29",
   listener_port: 3000,
+  # Range below represents milliseconds
+  common_random_backoff: 150..300,
   roles: [
     # {<AtomName>, <Module>, <Initial Args>, <InstanceCount>}
     {:test, Beethoven.TestRole, [arg1: "arg1"], 1}
