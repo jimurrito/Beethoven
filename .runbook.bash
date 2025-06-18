0438a0f071a7cc0d4450a0f97d43bac9201e5214
jimurrito <jimurrito@gmail.com>
2025-06-18 11:30:21 -0500

    Bug fixes

--- .runbook.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Publish to Hex - ENSURE YOU ITERATE THE VERSION OF THE PACKAGE!
-HEX_API_KEY=95a484b47ed2829c9b4c5b2a602be6f3 mix hex.publish --yes
-
-# if you need a new key, run this. Key was saved to Bitwarden
-#mix hex.user auth
-#mix hex.user key generate --key-name publish-cli --permission api:write
